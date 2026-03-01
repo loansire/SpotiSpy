e4deda3423b4d816acdcce2bfce21efc79890fe0
loansire <loan.sire@corys.fr>
2026-03-01 07:51:20 +0100

    change config

--- bot/config.py
+++ bot/config.py
@@ -14,9 +14,9 @@ ANNOUNCE_CHANNEL   = int(os.getenv("ANNOUNCE_CHANNEL", 0))
 # ─── PATHS & SETTINGS ─────────────────────────────────────────────────────────
 DATA_FILE          = "tracked.json"
 QUEUE_FILE                = "queue.json"
-RATE_LIMIT_PING_INTERVAL  = 3    #600 secondes entre les logs de progression du rate limit
+RATE_LIMIT_PING_INTERVAL  = 600    #600 secondes entre les logs de progression du rate limit
 QUEUE_REQUEST_DELAY       = 2      # secondes entre chaque requête lors du traitement de la file
 CHECK_INTERVAL_H   = 1        # intervalle de vérification en heures
-STARTUP_DELAY_S    = 5       # délai avant le premier cycle en secondes
+STARTUP_DELAY_S    = 0       # délai avant le premier cycle en secondes
 LOG_FILE           = "bot.log"
 SLEEP_THRESHOLD    = 10      # nb d'artistes à partir duquel on active le délai entre requêtes
